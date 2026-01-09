a97a567c4db01ba3632305a92656430688f8d1ab
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2026-01-09 10:39:47 +0100

    fixup! ~ wip - fix deprecations (misnomer, to review)

--- plugins/scripting/scripting-compiler/src/org/jetbrains/kotlin/scripting/compiler/plugin/FirScriptingCompilerExtensionRegistrar.kt
+++ plugins/scripting/scripting-compiler/src/org/jetbrains/kotlin/scripting/compiler/plugin/FirScriptingCompilerExtensionRegistrar.kt
@@ -6,17 +6,12 @@
 package org.jetbrains.kotlin.scripting.compiler.plugin
 
 import org.jetbrains.kotlin.config.CompilerConfiguration
-import org.jetbrains.kotlin.config.scriptingHostConfiguration
 import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrar
-import org.jetbrains.kotlin.scripting.compiler.plugin.definitions.*
 import org.jetbrains.kotlin.scripting.compiler.plugin.services.Fir2IrScriptConfiguratorExtensionImpl
 import org.jetbrains.kotlin.scripting.compiler.plugin.services.FirScriptConfiguratorExtensionImpl
 import org.jetbrains.kotlin.scripting.compiler.plugin.services.FirScriptDefinitionProviderService
 import org.jetbrains.kotlin.scripting.compiler.plugin.services.FirScriptResolutionConfigurationExtensionImpl
 import org.jetbrains.kotlin.scripting.configuration.ScriptingConfigurationKeys
-import kotlin.script.experimental.host.ScriptingHostConfiguration
-import kotlin.script.experimental.host.with
-import kotlin.script.experimental.jvm.defaultJvmScriptingHostConfiguration
 
 class FirScriptingCompilerExtensionRegistrar(
     private val compilerConfiguration: CompilerConfiguration
