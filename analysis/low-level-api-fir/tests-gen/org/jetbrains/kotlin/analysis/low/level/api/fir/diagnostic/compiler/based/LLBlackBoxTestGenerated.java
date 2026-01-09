4184d89f8194f7e0ab9595fc2dac6837f5cb36b9
Igor Chevdar <igor.chevdar@jetbrains.com>
2026-01-09 11:50:04 +0200

    [box-tests] Added a reproducer for #KT-83308

--- analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLBlackBoxTestGenerated.java
+++ analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLBlackBoxTestGenerated.java
@@ -35422,6 +35422,12 @@ public class LLBlackBoxTestGenerated extends AbstractLLBlackBoxTest {
     public void testInterface0() {
       runTest("compiler/testData/codegen/box/interfaceCallsNCasts/interface0.kt");
     }
+
+    @Test
+    @TestMetadata("kt83308.kt")
+    public void testKt83308() {
+      runTest("compiler/testData/codegen/box/interfaceCallsNCasts/kt83308.kt");
+    }
   }
 
   @Nested
