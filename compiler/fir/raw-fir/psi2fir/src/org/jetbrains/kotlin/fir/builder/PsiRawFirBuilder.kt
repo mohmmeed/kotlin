eb0b4c0e348844e3df272b504cbb6e3ac493cbe6
Brian Norman <brian.norman@jetbrains.com>
2026-01-15 15:01:42 -0600

    [REPL] Snippet eval function should be suspended
    To support suspend function calls from REPL snippets, the generated eval
    function itself needs to be suspend.
    
    ^KT-74683 Fixed

--- compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/PsiRawFirBuilder.kt
+++ compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/PsiRawFirBuilder.kt
@@ -1527,7 +1527,7 @@ open class PsiRawFirBuilder(
                     name = evalName
                     symbol = evalSymbol
                     dispatchReceiverType = currentDispatchReceiverType()
-                    status = FirDeclarationStatusImpl(Visibilities.Public, Modality.FINAL)
+                    status = FirDeclarationStatusImpl(Visibilities.Public, Modality.FINAL).apply { isSuspend = true }
                     returnTypeRef = implicitUnitType
                     isLocal = false
 
